43eb606ecd3feec4f65a3061b3b01a024b5dc720
Gokulrajkmv <64945548+Gokulrajkmv@users.noreply.github.com>
2021-07-22 12:03:13 +0530

    Add files via upload
    model deployment process

---
+++ flask/app.py
@@ -0,0 +1,33 @@
+import numpy as np
+import pandas as pd
+from flask import Flask, request, render_template
+import pickle
+
+# initialise the Flask
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('Home.html')# html front end
+
+def ValuePredictor(to_predict_list):
+    to_predict = np.array(to_predict_list).reshape(1, 6)
+    loaded_model = pickle.load(open("grading_boost_used_car.pkl", "rb"))
+    result = loaded_model.predict(to_predict)
+    return result[0]
+
+
+
+@app.route('/result',methods=['POST'])
+def result():
+    if request.method== 'POST':
+        to_predict_list = request.form.to_dict()
+        to_predict_list = list(to_predict_list.values())
+        #to_predict_list = list(map(int, to_predict_list))
+        result = ValuePredictor(to_predict_list)
+
+        return render_template('result.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
